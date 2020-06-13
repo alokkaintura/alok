e6260fd045a5c46cb9a27d340e079187bdff9067
alokkaintura <66751016+alokkaintura@users.noreply.github.com>
2020-06-13 15:28:33 +0530

    Update web.php

--- web.php
+++ web.php
@@ -1,60 +1,10 @@
+echo ' Welcome To My World ';
+
 <?php
-echo'
+ echo '
 <html>
-	<head>
-		<title>Multi Hybrid</title>
-		
-	</head>
-	<body background="https://raw.githubusercontent.com/alokkaintura/image/master/akp.jpg">
-	     
-	 <center><a href="#">Home</a>
-	 	<a href="#"><a href="Courcs.html">Cources</a></a>
-	     			<a href="#"><a href="pics.html">Gallery</a></a>
-	     			<a href="#"><a href="about.html">About</a></a>
-	     			<a href="#"><a href="Courcs.html">Contect</a></a>
-	     		
-	 </header>
-	     
-	<u><font size=80 color="red"><marquee bahaaviour="slide" direction="right">HYBRID MULTI CLOUD</marquee></font></u></center><br>
-	<fieldset><center>
-	<img src = "https://raw.githubusercontent.com/alokkaintura/image/master/vimal.jpg" width="200" height="200" />
+       <center> <img src="https://github.com/alokkaintura/alualu.git"  width="300" height="400">  </center>
 	
-
-		</br><font size=3 color="white"><u>VIMAL DAGA SIR</u></font></br>
-	</br></br><form>
-	<b>Username-<input type="text"</b><br><br>
-	<b>Password-<input type="password"></b><br><br>
-	<b>D.O.B-   <input type="date"></b><br><br>
-	<b>Gender-<input type="radio"name="r1">Male
-	<input type="radio"name="r1">Female</b><br><br>
-	
-
-	 
-	 <select>
-	 	<b>State</b>-<option>Jammu Kashmir</option>
-	 	      <option>Uttarakhand</option>
-	 	      <option>Assam</option>
-	 	      <option>Gujrat</option>
-	 	      <option>Maharashtra</option></select><br><br>
-	<b>Language-<input type="checkbox">Hindi
-	            <input type="checkbox">English</b><br><br>
-	<b>Address-<textarea rows="2 colum="2></textarea><br><br>
-		<b>Search:<input list="my">
-			 <datalist id="my">
-			 	<option value="alok">
-			 		<option value="himanshu">
-			 			<option value="farid">
-			 				<option value="abhishek">
-			 					<option value="kush">
-			 						<br><br>
-			 </datalist>
-	
-
-	<input type="Submit" value="Register">
-	<input type="reset"><br><br>
-	</center>
-	</fieldset>
-	</body>
-	</html>
- ';   
+</html>
+';	
 ?>
